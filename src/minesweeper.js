dce80e90002a25d69580fb51e790db7b9b2959ad
a_cumulus <timwilburn8@gmail.com>
2017-12-24 01:27:40 -0500

    replaced all old code, added printBoard function to print the board array

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,17 @@
-//variable creates 1 row of 3 columns, each seperated by a vertical line
-const blankLine = '   |   |   ';
-//prints a message and the visual representation of the 3x3 game board
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-//hard coded example of what the board will look like when a player selects a square
-const guessLine =  ' 1 |   |   ';
-//represents what the board will look like when a player reveals a bomb
-const bombLine = '   | B |   ';
-//prints message and visual representation
-console.log('This is what a board with a guess and a bomb would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const printBoard = (board ) => {
+  console.log('The current board is: ');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
