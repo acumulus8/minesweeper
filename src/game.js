d19a7483829ec4172be39d0a1e4c892e7818659b
a_cumulus <timwilburn8@gmail.com>
2018-01-21 19:12:32 -0500

    add game.js, exported Board class and import into game.js, delete original minesweeper.js

---
+++ src/game.js
@@ -0,0 +1,26 @@
+import {Board} from './board';
+
+class Game {
+    constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+      this._numberOfRows = numberOfRows;
+      this._numberOfColumns = numberOfColumns;
+      this._numberOfBombs = numberOfBombs;
+      this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
+    }
+  
+    playMove(rowIndex, columnIndex) {
+      this._board.flipTile(rowIndex, columnIndex);
+      if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
+        console.log('Oh no! You hit a bomb! Game Over...');
+        this._board.print();
+      } else if (this._board.hasSafeTiles() === false) {
+        console.log('Congratulations! You won!');
+      } else {
+        console.log('Current board: ');
+        this._board.print();
+      }
+    }
+}
+
+const g = new Game(3, 3, 3);
+g.playMove(0, 2);
